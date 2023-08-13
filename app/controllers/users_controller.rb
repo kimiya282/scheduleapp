512075ba739978277e0008aed2da4ed9ef85bb13
kimiya282 <kimiya19971026@gmail.com>
2023-08-13 22:12:44 +0900

    フロントページ作業中

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
+# ここより下の各アクションを追加しましょう
+def new
+  @users = User.new
+end
+
+def create
+end
+
+def show
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+end
